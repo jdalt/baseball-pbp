ab0fa4013d4f26ecb028fc4829101e26e574663d
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-23 11:38:22 -0500

    add karma to dev

--- dev-tmux.sh
+++ dev-tmux.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 tmux split-window -v
+tmux split-window -v
 
 tmux send-keys -t 0 'node server.js' Enter
 tmux send-keys -t 1 'grunt' Enter
+tmux send-keys -t 2 'karma start' Enter
 
