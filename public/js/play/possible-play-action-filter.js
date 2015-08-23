cd4e597832833a3c7b0aadb1ad0156f21b8eb7eb
Jacob Dalton <jacob.dalton@sportngin.com>
2015-08-23 01:38:59 -0500

    return to ret to return

--- public/js/play/possible-play-action-filter.js
+++ public/js/play/possible-play-action-filter.js
@@ -3,11 +3,10 @@ angular.module('play')
   return function(allPlayActions) {
     var numRunners = GameState.runners().length
     var remainingOuts = 3 - GameState.outs()
-    var ret = _.select(allPlayActions, function(playAction) {
+    return _.select(allPlayActions, function(playAction) {
       if(playAction.outs > remainingOuts) return false
       if(playAction.requiredRunners > numRunners) return false
       return true
     })
-    return ret
   }
 })
